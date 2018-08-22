2e285572d6a2007f6000681fb8aa56446d90f145
JohnAnthony Dobson <jcdobson2@gmail.com>
2018-08-22 15:38:13 -0400

    Modified program to pass reference of variable to change, saving progress...

--- test.c
+++ test.c
@@ -1,17 +1,45 @@
 #include <stdio.h>
 
+void myFunction(int* x, int* y) { //very import to add "int*" when passing addresses
+	*x = *x+2;
+	*y = *y-3;
+}
+
 int main() { 
 	int a, b;
+	int *x, *y;
+
+	printf("Enter two ints: ");
 	scanf("%d %d", &a, &b);
-	void myFunction(a,b);
+
+	x = &a;
+	y = &b;
+
+	
+	myFunction(x, y);
+	
+	printf("a is =%d, b is =%d\n", *x,*y);
 	printf("a is =%d, b is =%d\n", a,b);
+
+	//testing what is printed 
+	/*int x = 4; 
+
+	int *y = &x;
+
+	printf("no pointer %d \n", x);
+
+	printf("pointer %d \n", *y);
+
+	printf("address %p\n", &x);*/
+
+
+
+
+
 	return 0;
 } 
 
-void myFunction(int a, int b) {
-	a = a+1;
-	b = b-1;
-}
+
 
 //input 7,8
 //output 
