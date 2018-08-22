1917a409cca0839b1b2c1403a6473f034645f938
JohnAnthony Dobson <jcdobson2@gmail.com>
2018-08-22 14:25:13 -0400

    Program that shows how c handles variable and memory segments

--- test.c
+++ test.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main() { 
-
-
+	int a, b;
+	scanf("%d %d", &a, &b);
+	void myFunction(a,b);
+	printf("a is =%d, b is =%d\n", a,b);
 	return 0;
 } 
+
+void myFunction(int a, int b) {
+	a = a+1;
+	b = b-1;
+}
+
+//input 7,8
+//output 
