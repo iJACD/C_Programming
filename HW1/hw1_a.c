f24e117c9f4d8036a0f97df7a9980d8dd50b33c0
JohnAnthony Dobson <jcdobson2@gmail.com>
2018-08-24 10:42:16 -0400

    Homework 1 assigment established

---
+++ HW1/hw1_a.c
@@ -0,0 +1,14 @@
+//HW1 Problem a
+//JohnAnthony Dobson
+//CS 3335
+//8/24/18
+/* Write a C-program to solve any problem that involves
+	i) data types char, int, and float
+	ii) scanf() to handle user input
+	iii) simple array (of any data type above)
+	iv) for loop to handle repetition (3-5 iterations)
+	v) if-then-else to make decision
+	vi) printf() to produce formatted output */
+
+#include <stdio.h>
+
